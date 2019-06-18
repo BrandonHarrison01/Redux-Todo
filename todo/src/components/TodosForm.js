4ae709a17458a87357d2dbadc1c43b4a8d0ae90f
BrandonHarrison01 <49922064+BrandonHarrison01@users.noreply.github.com>
2019-06-18 14:30:49 -0600

    WIP initial setup and components layed out

---
+++ todo/src/components/TodosForm.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function TodosForm () {
+    return(
+        <form>
+            <input />
+            <button>Submit</button>
+        </form>
+    )
+}
+
+export default TodosForm
